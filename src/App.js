6d8172a1590f699c52cf7557c80950924fdd3b45
Benz Inthra <benzinthra@gmail.com>
2018-06-21 15:39:18 +1000

    init weather-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// jshint esversion:6
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div> 
+				<h1>Hello Universe!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
